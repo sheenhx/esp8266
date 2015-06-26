e9035d2a0bb511a47238ffc01160456a51e863de
sheenhx <hx.sheen@gmail.com>
2015-06-26 15:22:40 +0200

    initial scripts for NodeMCU firmware

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 gpio.mode(0,gpio.OUTPUT)
 dofile("obcfg.lua") --load onboarding configuration
 
-if OB == "y" then
+if OB == "y" then -- check the On boarding flag
 	print(node.heap())
 	dofile("relayrob.lc")
 else
-	dofile("mqtt.lc")
+	dofile("relayrmqtt.lc")
 end
 
 tmr.alarm(2, 400, 1, function()
 	if wifi.sta.getip() == nil then
-		gpio.write(0,1 - gpio.read(0))
+		gpio.write(0,1 - gpio.read(0)) --toggle the LED if WIFI is not connected
 	end
 end) 
 
